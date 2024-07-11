98ea2278b041000b735cd3a3a45830a1952e6301
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-07-11 12:33:12 +0500

    Update Q2.js

--- JavaScript/Day-2 (Practice)/Q2.js
+++ JavaScript/Day-2 (Practice)/Q2.js
@@ -1,1 +1,23 @@
+/* Question # 2 Define a function named longestWord that takes string and returns  longest word. For example:
+input : "This is Optimusfox js internship class"
+ 
+output : "Optimusfox */
 
+function longestWord(str)
+{
+    const words = str.split(" ");
+    let longest = "";
+  
+    words.forEach(word => {
+        if (word.length > longest.length)
+        {
+            longest = word;
+        }
+    });
+    
+    return longest;
+}
+
+const inputString = "This is Optimusfox js internship class";
+const longest = longestWord(inputString);
+console.log(longest);
