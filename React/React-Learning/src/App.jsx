30f8538944c4c3fc7347826af0af10720150d5ab
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-07-17 19:37:08 +0500

    Update App.jsx

--- React/React-Learning/src/App.jsx
+++ React/React-Learning/src/App.jsx
@@ -1,1 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import HomePage from "./components/HomePage";
+import UpdateData from "./components/UpdateData";
+import ContactInfo from "./components/ContactInfo";
+import Parent from "./components/Parent";
+
+function Website() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        {/* <HomePage/> */}
+        <HomePage text={{ name: "Fatima" }} />
+        <UpdateData />
+        <ContactInfo />
+        <Parent />
+      </div>
+    </>
+  );
+}
+
+export default Website;
 
