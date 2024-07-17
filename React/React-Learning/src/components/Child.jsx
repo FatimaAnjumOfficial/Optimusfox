533e9d29ce6aa0de022c0f699d3519d2b86af5cd
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-07-17 19:38:23 +0500

    Update Child.jsx

--- React/React-Learning/src/components/Child.jsx
+++ React/React-Learning/src/components/Child.jsx
@@ -1,1 +1,16 @@
+import React from "react";
 
+function Child({ showMessage }) {
+  const handleClick = () => {
+    showMessage("Hello from Child Component!");
+  };
+
+  return (
+    <div>
+      <h3>Child Component</h3>
+      <button onClick={handleClick}>Send Message to Parent</button>
+    </div>
+  );
+}
+
+export default Child;
