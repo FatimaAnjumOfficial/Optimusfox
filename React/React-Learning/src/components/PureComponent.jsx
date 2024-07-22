0e9c3e1152a31c7f1a6f7d27461e7cfadf6ca2df
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-07-22 20:11:43 +0500

    Update PureComponent.jsx

--- React/React-Learning/src/components/PureComponent.jsx
+++ React/React-Learning/src/components/PureComponent.jsx
@@ -1,30 +1,15 @@
 import React from "react";
-import Counter from "./Counter";
 
 //eg. increment hr br +1 kr deta h (value change)
 //value same ho or button pe br br click kren gy tw rendering hogi.Pure Component br br rendoring nhi krta
 
-class PureComponent extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 1,
-    };
-  }
-  render() {
-    return (
-      <div>
-        <Counter count={this.state.count} />
-        <button
-          onClick={() => {
-            this.setState({ count: 1 });
-          }}
-        >
-          Update Count
-        </button>
-      </div>
-    );
-  }
-}
+const PureComponent = ({ title, content }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{content}</p>
+    </div>
+  );
+};
 
-export default PureComponent;
+export default React.memo(PureComponent);
