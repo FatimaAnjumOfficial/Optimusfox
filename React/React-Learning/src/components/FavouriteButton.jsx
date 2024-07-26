fc965d2d171874cea488a770f77ac88a8def4f44
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-07-26 19:45:48 +0500

    Update FavouriteButton.jsx

--- React/React-Learning/src/components/FavouriteButton.jsx
+++ React/React-Learning/src/components/FavouriteButton.jsx
@@ -4,7 +4,7 @@ import { useFavorites } from "./FavoritesContext";
 function FavouriteButton({ participant, isFavorite }) {
   const { toggleFavorite } = useFavorites();
   const fav = isFavorite ? "❤️" : "♡";
-
+  
   return <button onClick={() => toggleFavorite(participant)}>{fav}</button>;
 }
 
