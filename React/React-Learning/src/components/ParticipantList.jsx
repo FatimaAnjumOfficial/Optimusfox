9599cf0bf4e85ee719e9125b01c39b4296fae483
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-07-28 23:35:01 +0500

    Update ParticipantList.jsx

--- React/React-Learning/src/components/ParticipantList.jsx
+++ React/React-Learning/src/components/ParticipantList.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import Card from "./Card";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const ParticipantList = ({ participants }) => {
+const ParticipantList = () => {
+  const [participants, setParticipants] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/participants")
+      .then((response) => {
+        setParticipants(response.data); //client k browser ko send kr rha  data
+      })
+      .catch((error) => {
+        console.error("Error fetching participants:", error);
+      });
+  }, []);
+
   return (
     <div className="participant-list">
       {participants.map((participant) => (
