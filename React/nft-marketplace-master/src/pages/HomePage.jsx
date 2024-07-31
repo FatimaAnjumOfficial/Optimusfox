0d4111a93fdc971547862de1a89aa760f8880393
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-07-31 18:33:23 +0500

    Update HomePage.jsx

--- React/nft-marketplace-master/src/pages/HomePage.jsx
+++ React/nft-marketplace-master/src/pages/HomePage.jsx
@@ -3,39 +3,17 @@ import { MarketPlaceFilters } from "../components/marketPlace/MarketPlaceFilters
 import { MarketPlaceHeader } from "../components/marketPlace/MarketPlaceHeader";
 import { MarketPlaceSubmenu } from "../components/marketPlace/MarketPlaceSubmenu";
 import { LayoutContainer } from "../layout/LayoutContainer";
+import AddButton from "./AddButton";
+import "./HomePage.css";
 
 export function HomePage() {
-  const [nfts, setNfts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchNfts = async () => {
-      try {
-        const response = await axios.get("http://localhost:4000/nfts");
-        setNfts(response.data);
-      } catch (error) {
-        setError("Failed to fetch NFTs.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchNfts();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
-  if (nfts.length === 0) return <p>No data available.</p>;
-
   return (
     <LayoutContainer>
       <MarketPlaceHeader />
       <MarketPlaceSubmenu />
+      <AddButton />
       <MarketPlaceFilters />
-      {nfts.map((nft) => (
-        <NFTSection key={nft.rank} {...nft} />
-      ))}
+      <NftSection />
     </LayoutContainer>
   );
 }
