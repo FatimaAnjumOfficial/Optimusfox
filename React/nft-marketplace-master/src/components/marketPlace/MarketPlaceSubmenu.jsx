969a7dca6ddd323ac7a2f17f50be247740070848
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-03 23:10:39 +0500

    Update MarketPlaceSubmenu.jsx

--- React/nft-marketplace-master/src/components/marketPlace/MarketPlaceSubmenu.jsx
+++ React/nft-marketplace-master/src/components/marketPlace/MarketPlaceSubmenu.jsx
@@ -1,51 +1,53 @@
-import {
-  Cross,
-  Diamond,
-  Discount,
-  Hand,
-  Image,
-  Search,
-} from '../../assets/Icons';
-import { Button } from '../shared/Button';
+import { Cross, Diamond, Discount, Hand, Image } from "../../assets/Icons";
+import { SharedButton } from "../shared/SharedButton";
+//import "../../pages/StylishButton.css";
+import Search from "../../pages/Search";
+import Button from "../../pages/Button";
 
 export function MarketPlaceSubmenu() {
   return (
-    <div className='py-6 flex md:gap-3 justify-between text-sm'>
-      <div className='flex bg-zinc-800 rounded-xl items-center p-3 gap-1 w-full'>
-        <Search />
-        <input type='search' placeholder='Search' className='bg-zinc-800' />
+    <>
+      <div>
+        <div>
+          {/*<Search />
+        <input type="search" placeholder="Search" className="bg-zinc-800" />*/}
+          <Search />
+        </div>
+        <br />
+        <div className="flex gap-3">
+          <SharedButton
+            className="md:flex hidden gap-2.5 rounded-xl px-[50px]  bg-secondary-danger items-center"
+            variant="ghost"
+          >
+            <Image />
+            <span>NFT</span>
+            <Cross />
+          </SharedButton>
+          <SharedButton
+            className="md:flex hidden gap-2.5 rounded-xl px-[50px] bg-zinc-800 items-center min-w-max"
+            variant="ghost"
+          >
+            <Discount />
+            <span>Discount Codes</span>
+          </SharedButton>
+
+          <SharedButton
+            className="xl:flex hidden gap-2.5 rounded-xl px-[50px] bg-zinc-800 items-center  min-w-max"
+            variant="ghost"
+          >
+            <Hand />
+            <span>Physical Items</span>
+          </SharedButton>
+          <Button
+            className="xl:flex hidden gap-2.5 rounded-xl px-[50px] bg-zinc-800 items-center  min-w-max"
+            variant="ghost"
+          >
+            <Diamond />
+            <span>Perks</span>
+          </Button>
+          <Button />
+        </div>
       </div>
-      <div className='flex gap-3'>
-        <Button
-          className='md:flex hidden gap-2.5 rounded-xl px-[50px]  bg-secondary-danger items-center'
-          variant='ghost'
-        >
-          <Image />
-          <span>NFT</span>
-          <Cross />
-        </Button>
-        <Button
-          className='md:flex hidden gap-2.5 rounded-xl px-[50px] bg-zinc-800 items-center min-w-max'
-          variant='ghost'
-        >
-          <Discount />
-          <span>Discount Codes</span>
-        </Button>
-        <Button
-          className='xl:flex hidden gap-2.5 rounded-xl px-[50px] bg-zinc-800 items-center  min-w-max'
-          variant='ghost'
-        >
-          <Hand />
-          <span>Physical Items</span>
-        </Button>
-        <Button
-          className='xl:flex hidden gap-2.5 rounded-xl px-[50px] bg-zinc-800 items-center  min-w-max'
-          variant='ghost'
-        >
-          <Diamond />
-          <span>Perks</span>
-        </Button>
-      </div>
-    </div>
+    </>
   );
 }
