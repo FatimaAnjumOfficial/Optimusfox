54e512a7ab8e2984d7034985b4f5445ed82fb100
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-03 23:34:53 +0500

    Update MarketPlaceHeader.jsx

--- React/nft-marketplace-master/src/components/marketPlace/MarketPlaceHeader.jsx
+++ React/nft-marketplace-master/src/components/marketPlace/MarketPlaceHeader.jsx
@@ -3,7 +3,7 @@ import { SharedButton } from "../shared/SharedButton";
 export function MarketPlaceHeader() {
   return (
     <div className="flex sm:justify-between sm:flex-row flex-col items-center md:gap-0 gap-5">
-      <h1 className="text-lg md:text-[28px] font-semibold">Marketplace</h1>
+      <h1 className="text-lg md:text-[28px] font-bold">Marketplace</h1>
       <div className="rounded-full flex items-center px-3 bg-zinc-800 text-xs w-[270px]">
         <SharedButton variant="ghost">Active</SharedButton>
         <SharedButton className="rounded-full px-3 bg-zinc-700" variant="ghost">
