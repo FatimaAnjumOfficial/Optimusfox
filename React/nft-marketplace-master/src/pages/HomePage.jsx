55670397287624fadad12ebc1ba19b18d6604275
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-04 00:24:25 +0500

    Update HomePage.jsx

--- React/nft-marketplace-master/src/pages/HomePage.jsx
+++ React/nft-marketplace-master/src/pages/HomePage.jsx
@@ -4,12 +4,14 @@ import { MarketPlaceHeader } from "../components/marketPlace/MarketPlaceHeader";
 import { MarketPlaceSubmenu } from "../components/marketPlace/MarketPlaceSubmenu";
 import { LayoutContainer } from "../layout/LayoutContainer";
 import "./HomePage.css";
+import Button from "./Button";
 
 export function HomePage() {
   return (
     <LayoutContainer>
       <MarketPlaceHeader />
       <MarketPlaceSubmenu />
+      <Button />
       <MarketPlaceFilters />
       <NftSection />
     </LayoutContainer>
