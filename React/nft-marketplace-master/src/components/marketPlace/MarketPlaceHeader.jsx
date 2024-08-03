a0d28c9147f0cc1764961f2faecbdea2e9c0cfb3
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-03 23:09:09 +0500

    Update MarketPlaceHeader.jsx

--- React/nft-marketplace-master/src/components/marketPlace/MarketPlaceHeader.jsx
+++ React/nft-marketplace-master/src/components/marketPlace/MarketPlaceHeader.jsx
@@ -1,15 +1,15 @@
-import { Button } from '../shared/Button';
+import { SharedButton } from "../shared/SharedButton";
 
 export function MarketPlaceHeader() {
   return (
-    <div className='flex sm:justify-between sm:flex-row flex-col items-center md:gap-0 gap-5'>
-      <h1 className='text-lg md:text-[28px] font-semibold'>Marketplace</h1>
-      <div className='rounded-full flex items-center px-3 bg-zinc-800 text-xs w-[270px]'>
-        <Button variant='ghost'>Active</Button>
-        <Button className='rounded-full px-3 bg-zinc-700' variant='ghost'>
+    <div className="flex sm:justify-between sm:flex-row flex-col items-center md:gap-0 gap-5">
+      <h1 className="text-lg md:text-[28px] font-semibold">Marketplace</h1>
+      <div className="rounded-full flex items-center px-3 bg-zinc-800 text-xs w-[270px]">
+        <SharedButton variant="ghost">Active</SharedButton>
+        <SharedButton className="rounded-full px-3 bg-zinc-700" variant="ghost">
           Recommended
-        </Button>
-        <Button variant='ghost'>All Products</Button>
+        </SharedButton>
+        <SharedButton variant="ghost">All Products</SharedButton>
       </div>
     </div>
   );
